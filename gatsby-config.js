86244ee8dde3b2049f8535020bf8ade7be9f056f
Luciano Júnior <luciannojunior@gmail.com>
2021-05-03 09:50:27 -0300

    Starting to implement filtering

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,6 +53,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-    'gatsby-plugin-postcss'
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-use-query-params'
   ],
 }
--- src/pages/job-search.tsx
+++ src/pages/job-search.tsx
@@ -1,4 +1,7 @@
-import React, { FC } from "react"
+import React, { FC, useMemo } from "react"
+import Select from 'react-select'
+import { useQueryParam, StringParam } from "use-query-params";
+
 import Header from "../components/Header"
 import PostingList from "../components/JobSearch/PostingList"
 import { useAllPostings } from "../hooks/useAllPostings"
@@ -6,9 +9,31 @@ import { useAllPostings } from "../hooks/useAllPostings"
 const JobSearch: FC = () => {
   const postings = useAllPostings()
 
+  const [selectedLocations, setSelectedLocations] = useQueryParam("locations", StringParam);
+
+  // All of this should be extracted to a Filter components
+  const agregate = useMemo(() => {
+    return postings && postings.reduce((acc, { location }) => {
+     if (!acc[location]){
+       acc[location] = 0
+     } 
+
+     acc[location]++ 
+
+     return acc
+    }, {})
+  }, [postings])
+
+  const locationOptions = useMemo(() => {
+    return agregate ? Object.keys(agregate).map(loc => ({
+      value: loc,
+      label: `${loc} (${agregate[loc]})`
+    })) : []
+  }, [agregate])
+
   const searchLabel = `We have 193 jobs in 26 locations in 29 areas of work in 7 seniority levels`
   return (
-    <div className="text-red-400">
+    <div>
       <Header />
       {/* Extract into Header maybe */}
       <div
@@ -25,7 +50,11 @@ const JobSearch: FC = () => {
         <h1 className="text-6xl text-gray-900">Write here your next future</h1>
       </div>
       {/* Extract into Filters */}
-      <div className="flex flex-col justify-center text-center">
+      <div className="flex flex-col justify-center items-center -mt-9 text-center">
+        <span>Location:</span>
+        <div className="w-6/12">
+          <Select options={locationOptions} setValue={(value) => console.log(value)} />
+        </div>
         <span className="pt-4 text-md text-black">{searchLabel}</span>
       </div>
       {postings?.length && <PostingList postings={postings} />}
