42d3da9695eab0a8ca06c5bf9480c0fa22ac479a
Luciano Júnior <luciannojunior@gmail.com>
2021-06-08 15:26:19 -0300

    Fixing redir and trimming title

--- src/components/JobSearch/PostingCard.tsx
+++ src/components/JobSearch/PostingCard.tsx
@@ -7,13 +7,15 @@ const PostingCard: FC<Props> = ({
   posting: { title, department, team, location, slug },
 }) => {
   const postingLink = `https://careers.vtex.com/postings/${slug}`
+  const postingTitle = title?.split('–').reverse().slice(1).reverse().join('–')
+
   return (
     <a href={postingLink} className="w-full">
       <div
         style={{ borderBottom: "1px solid #ccc" }}
         className="posting-card mt-2 w-full p-6 flex flex-col border-gray-300 border-solid hover:shadow-lg cursor-pointer"
       >
-        <p className="posting-card-title">{title}</p>
+        <p className="posting-card-title">{postingTitle}</p>
         <span className="posting-card-team">{`${team} - ${department}`}</span>
         <span className="posting-card-location">{location}</span>
         <a
