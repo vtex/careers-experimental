3e39f93012081585738b99690f13d4a0f6955ecb
edinhoo <eamedeirosrj@gmail.com>
2022-03-17 12:11:17 -0300

    remove console.log

--- src/pages/job-search.tsx
+++ src/pages/job-search.tsx
@@ -101,8 +101,6 @@ const JobSearch: FC = () => {
     return () => clearInterval(interval);
   }, [hidePlaceholder, animationInterval, windowSize?.width]);
 
-  useEffect(() => { console.log('animationIndex', animationIndex)}, [animationIndex]);
-
   useEffect(() => {
     if (selectedQueryContinent && selectedQueryContinent.length) {
       setSelectedContinent(selectedQueryContinent.split(','));
